f15adca5ac83b3ca50f400e5dea5f0bc157d61d4
Keith MacKay <keith@keithmackay.com>
2021-11-15 12:51:30 -0500

    Brew formula update for all version v1.8.8

--- Formula/all.rb
+++ Formula/all.rb
@@ -5,21 +5,21 @@
 class All < Formula
   desc "Filesystem CLI Tools"
   homepage ""
-  version "1.8.7"
+  version "1.8.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kamackay/all/releases/download/v1.8.7/all_1.8.7_Darwin_x86_64.tar.gz"
-      sha256 "60da18da509369fe1b9cadad7ce64af04b5187bf0f2fea3e996e27e59d547b10"
+      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Darwin_x86_64.tar.gz"
+      sha256 "1b9e044a579837f8115509f88c5eb5189b40b2c26b4b2b6a79a07e2b4f6764ff"
 
       def install
         bin.install "all"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kamackay/all/releases/download/v1.8.7/all_1.8.7_Darwin_arm64.tar.gz"
-      sha256 "e8e2bbc4858a28a3d60a3515f83c6cc1dae483522965b9af64d267573a4b1517"
+      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Darwin_arm64.tar.gz"
+      sha256 "81cf73d9ae551bca2617916915d47b74c12e15a817c137b681aee34d435d73f1"
 
       def install
         bin.install "all"
@@ -28,17 +28,17 @@ class All < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kamackay/all/releases/download/v1.8.7/all_1.8.7_Linux_arm64.tar.gz"
-      sha256 "a63c1bb566e624ada9db0885944be30addb5b628f75845e2db042ae1f6e1282a"
+    if Hardware::CPU.intel?
+      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Linux_x86_64.tar.gz"
+      sha256 "eb0721f3cd24bcf32870d324ec50cb291d6f20c0de7bc29a8307564763a78187"
 
       def install
         bin.install "all"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kamackay/all/releases/download/v1.8.7/all_1.8.7_Linux_x86_64.tar.gz"
-      sha256 "a6a4228f691f89c7226d099a7c63527eb8dba36025dc1ad3e5876deb59e939f5"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Linux_arm64.tar.gz"
+      sha256 "28fa27e93736f94d91e1f525cbc44c46c7d2bb8297dce867bbf96b307da8655c"
 
       def install
         bin.install "all"
