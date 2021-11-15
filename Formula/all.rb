c53c382e07453517f84cd551ee0a5b344ff7e34a
Keith MacKay <keith@keithmackay.com>
2021-11-15 15:42:24 -0500

    Brew formula update for all version v1.8.9

--- Formula/all.rb
+++ Formula/all.rb
@@ -5,21 +5,21 @@
 class All < Formula
   desc "Filesystem CLI Tools"
   homepage ""
-  version "1.8.8"
+  version "1.8.9"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Darwin_x86_64.tar.gz"
-      sha256 "1b9e044a579837f8115509f88c5eb5189b40b2c26b4b2b6a79a07e2b4f6764ff"
+    if Hardware::CPU.arm?
+      url "https://github.com/kamackay/all/releases/download/v1.8.9/all_1.8.9_Darwin_arm64.tar.gz"
+      sha256 "a4d934974f4c6965dfeb56e58f2480e90a6ffc86b559604bd94143591bae9596"
 
       def install
         bin.install "all"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Darwin_arm64.tar.gz"
-      sha256 "81cf73d9ae551bca2617916915d47b74c12e15a817c137b681aee34d435d73f1"
+    if Hardware::CPU.intel?
+      url "https://github.com/kamackay/all/releases/download/v1.8.9/all_1.8.9_Darwin_x86_64.tar.gz"
+      sha256 "a3314f33f4c5fdf7e989713734bcecd835cfe6db553f62621f18258f330a5834"
 
       def install
         bin.install "all"
@@ -29,16 +29,16 @@ class All < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Linux_x86_64.tar.gz"
-      sha256 "eb0721f3cd24bcf32870d324ec50cb291d6f20c0de7bc29a8307564763a78187"
+      url "https://github.com/kamackay/all/releases/download/v1.8.9/all_1.8.9_Linux_x86_64.tar.gz"
+      sha256 "bef3cafc776afa136ff372a9fd0c9c2898a4946374aeafccc86f46c3c370d5fa"
 
       def install
         bin.install "all"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kamackay/all/releases/download/v1.8.8/all_1.8.8_Linux_arm64.tar.gz"
-      sha256 "28fa27e93736f94d91e1f525cbc44c46c7d2bb8297dce867bbf96b307da8655c"
+      url "https://github.com/kamackay/all/releases/download/v1.8.9/all_1.8.9_Linux_arm64.tar.gz"
+      sha256 "4a08a10d53726fcd9ec03d26992c906f82d513e294a347f39ae76d043ae31b45"
 
       def install
         bin.install "all"
