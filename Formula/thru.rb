642f78d48295f5a41c678e1fa7e20849494b1581
Keith MacKay <keith@keithmackay.com>
2022-05-10 16:21:21 -0400

    Brew formula update for thru version v1.0.3

--- Formula/thru.rb
+++ Formula/thru.rb
@@ -5,21 +5,21 @@
 class Thru < Formula
   desc "CLI throughput tools"
   homepage ""
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kamackay/thru/releases/download/v1.0.2/thru_1.0.2_Darwin_x86_64.tar.gz"
-      sha256 "332ce9ac095959cf0161f87bcf9620cc3fdd465ece44ecee04211a6a0f6aa5e8"
+      url "https://github.com/kamackay/thru/releases/download/v1.0.3/thru_1.0.3_Darwin_x86_64.tar.gz"
+      sha256 "b5b679218f9379ed8b032bf522511fd1cd1c01b0e7effa74357796267fef2829"
 
       def install
         bin.install "thru"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kamackay/thru/releases/download/v1.0.2/thru_1.0.2_Darwin_arm64.tar.gz"
-      sha256 "b9c42499173a45ddf268f25503bf802fa6e118ac73afba848032a0437cea80cc"
+      url "https://github.com/kamackay/thru/releases/download/v1.0.3/thru_1.0.3_Darwin_arm64.tar.gz"
+      sha256 "09bc7085ce7e0eadd3859b7909400acb296c3eec0aca2acddd8a1fb9d698a545"
 
       def install
         bin.install "thru"
@@ -28,17 +28,17 @@ class Thru < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kamackay/thru/releases/download/v1.0.2/thru_1.0.2_Linux_arm64.tar.gz"
-      sha256 "b5ee3a0b7c6d75aca173057ca7f1e4f7b09e6d7ff0357c3debd8f80b69b4ba44"
+    if Hardware::CPU.intel?
+      url "https://github.com/kamackay/thru/releases/download/v1.0.3/thru_1.0.3_Linux_x86_64.tar.gz"
+      sha256 "9c2afd36154dbcfa1a430f0b63fdc7149ae29bb831638d9eab9ec1fb499b78df"
 
       def install
         bin.install "thru"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kamackay/thru/releases/download/v1.0.2/thru_1.0.2_Linux_x86_64.tar.gz"
-      sha256 "c1435c1997b94944cba3af128ff723445e1c23facd8ef470405ede50fad88669"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kamackay/thru/releases/download/v1.0.3/thru_1.0.3_Linux_arm64.tar.gz"
+      sha256 "5ff59171200f88070c97db1727ee1eb57f610a20e88e87872b76072ebc486476"
 
       def install
         bin.install "thru"
